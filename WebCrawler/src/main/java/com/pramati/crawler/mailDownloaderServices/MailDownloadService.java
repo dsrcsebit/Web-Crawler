83a7c202940ece1087f4e78f8c77f8596137f22e
deepak.singh@imaginea.com <deepaks@PRINHYLTPHP0404>
2015-02-12 11:31:33 +0530

    learning

--- WebCrawler/src/main/java/com/pramati/crawler/mailDownloaderServices/MailDownloadService.java
+++ WebCrawler/src/main/java/com/pramati/crawler/mailDownloaderServices/MailDownloadService.java
@@ -13,17 +13,17 @@ import com.pramati.crawler.uti.Utility;
 public class MailDownloadService implements MailDownloader {
 
 	String URL;
-	public static AtomicInteger i=new AtomicInteger();
+	public static AtomicInteger i = new AtomicInteger();
 
 	public MailDownloadService(String uRL) {
 		super();
 		URL = uRL;
 	}
 
-	public boolean saveMailFromURL() throws IOException {
+	public  boolean  saveMailFromURL() throws IOException {
 		HTMLRequester hreq = new HTMLRequestService();
-		Set<String> mailURL = hreq.htmlURLExtractor(URL+"?0", "table#msglist", "");
-		System.out.println("Mail URL after param" + mailURL.size());
+		Set<String> mailURL = hreq.htmlURLExtractor(URL + "?0",
+				"table#msglist", "");
 		Set<String> temp = new HashSet<String>();
 		int counter = 1;
 		temp.addAll(mailURL);
@@ -32,16 +32,13 @@ public class MailDownloadService implements MailDownloader {
 			temp.clear();
 			temp = hreq.htmlURLExtractor(URL + "?" + counter++,
 					"table#msglist", "");
-			
-			if (temp != null && temp.size()>1) {
-				if(counter==3)
-				System.out.println("Temp Size" +temp);
-				System.out.println("Temp Size" +temp.size());
+			//temp.remove(o)
+
+			if (temp != null && temp.size() > 1) {
 				mailURL.addAll(temp);
 			}
 		}
-		i.addAndGet(mailURL.size());
-
+		System.out.println(mailURL.size());
 		Iterator<String> it = mailURL.iterator();
 		while (it.hasNext()) {
 			// System.out.println(mailURL.size() + "mailURLs are"+mailURL );
@@ -50,13 +47,20 @@ public class MailDownloadService implements MailDownloader {
 			if (testURL.indexOf("%") != -1) {
 				testURL = testURL.substring(0, testURL.indexOf("%") - 1)
 						+ "/raw/" + testURL.substring(testURL.indexOf("%"));
-				String fileName=testURL.substring(testURL.indexOf("%"))+testURL.hashCode();
-				Utility.writeToFile(hreq.linkParser(testURL),fileName
-						);
+				String fileName = testURL.substring(testURL.indexOf("%"))
+						+ testURL.hashCode();
+				try
+				{
+					Utility.writeToFile(hreq.linkParser(testURL), fileName+System.currentTimeMillis());
+				}
+				catch(Exception e){
+					Utility.writeToFile(hreq.linkParser(testURL), fileName+System.currentTimeMillis());
+				}
 			} else {
-				String fileName=String.valueOf(System.currentTimeMillis())+testURL.hashCode();
-				Utility.writeToFile(hreq.linkParser(testURL),fileName
-						);
+				//String fileName = String.valueOf(System.currentTimeMillis())
+						//+ testURL;
+				System.out.println(testURL);
+				Utility.writeToFile(hreq.linkParser(testURL), testURL);
 			}
 			// System.out.println(hreq.linkParser(testURL));
 		}
